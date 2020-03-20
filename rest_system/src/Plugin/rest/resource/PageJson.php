981c1014da2718f05ab88d8770a9bd332df501a1
Abdul Vahab <abdul.drupal@gmail.com>
2020-03-20 11:40:52 +0530

    Updated plugin

--- rest_system/src/Plugin/rest/resource/PageJson.php
+++ rest_system/src/Plugin/rest/resource/PageJson.php
@@ -4,6 +4,8 @@ namespace Drupal\rest_system\Plugin\rest\resource;
 
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
 /**
  * Provides a Page Json Resource
  *
@@ -21,10 +23,36 @@ class PageJson extends ResourceBase {
    * Responds to entity GET requests.
    * @return \Drupal\rest\ResourceResponse
    */
-  public function get() {
-      
-    $response = ['message' => 'Hello, this is a rest service'];
-    return new ResourceResponse($response);
+  public function get($apikey, $nid) {
+    $config_factory = \Drupal::configFactory();
+    $config = $config_factory->get('system.site');
+    $nid = $nid ?? "";
+   if($config->get('siteapikey') != $apikey && empty($nid)){
+        $response = ['message' => "access denied"];
+        return new ResourceResponse($response);
+    }
+    elseif (!$nid) {
+        throw new BadRequestHttpException("page id not available.");
+      }
+    elseif($config->get('siteapikey') != $apikey){
+        throw new BadRequestHttpException("Invalid Api key??");
+    }else{
+    try{
+        $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+        $node = $node_storage->load($nid);
+        if(!is_null($node)){
+            $response = ['node' => $node->toArray()];
+        }else{
+            $response = ['message' => "access denied"];
+        }
+        return new ResourceResponse($response);
+    }
+    catch (RequestException $exception) {
+        $response = ['message' => $exception->getMessage()];
+        return new ResourceResponse($response);
+      }
+    }
+
   }
 
 }
